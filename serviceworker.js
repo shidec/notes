d5135184c45a4d403830aeef7fda57fd254c2017
shidec <shidec00@yahoo.com>
2023-05-02 06:52:54 +0700

    lighthouse fixes

--- serviceworker.js
+++ serviceworker.js
@@ -6,3 +6,17 @@ self.addEventListener("install", event => {
  self.addEventListener("activate", event => {
     console.log("Service worker activated");
  });
+
+ self.addEventListener('fetch', function(event) {
+   event.respondWith(async function() {
+      try{
+        var res = await fetch(event.request);
+        var cache = await caches.open('cache');
+        cache.put(event.request.url, res.clone());
+        return res;
+      }
+      catch(error){
+        return caches.match(event.request);
+       }
+     }());
+ });
