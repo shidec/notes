5667d35c576b998b809c81c692388305d2f65f43
shidec <shidec00@yahoo.com>
2023-05-01 22:00:42 +0700

    initial commit

---
+++ js/Note.js
@@ -0,0 +1,88 @@
+class Note{
+
+    constructor(data, container, editItem, updatePositionItem, deleteItem){
+        this.id = data.id;
+        this.text = data.text;
+        this.backgroundColor = data.backgroundColor;
+        this.x = data.x;
+        this.y = data.y;
+        this.container = container;
+        this.editItem = editItem;
+        this.updatePositionItem = updatePositionItem;
+        this.deleteItem = deleteItem;
+
+        this.startX = 0;
+        this.startY = 0;
+
+        this.newX = this.x;
+        this.newY = this.y;
+    }
+
+    render(){
+        var self = this;
+        const containerNote = document.createElement('div');
+        containerNote.className = 'container-note';
+        containerNote.style.backgroundColor = self.backgroundColor;
+        containerNote.style.left = self.x + 'px';
+        containerNote.style.top = self.y + 'px';
+        
+        const toolbarNote = document.createElement('div');
+        toolbarNote.className = 'toolbar-note';
+        toolbarNote.addEventListener('mousedown', function(e){
+            this.startX = e.pageX;
+            this.startY = e.pageY;
+        });
+
+        toolbarNote.addEventListener('mousemove', function(e){
+            if(this.startX > 0 && this.startY > 0){
+                self.newX = self.x - this.startX + e.pageX;
+                self.newY = self.y - this.startY + e.pageY;
+
+                containerNote.style.left = self.newX + 'px';
+                containerNote.style.top = self.newY + 'px';
+            }
+        });
+
+        toolbarNote.addEventListener('mouseup', function(e){
+            self.x = self.newX;
+            self.y = self.newY;
+
+            self.updatePositionItem(self.id);
+            this.startX = 0;
+            this.startY = 0;
+        });
+
+        const btnNoteDelete = new Image(24, 24);
+        btnNoteDelete.src = 'img/delete.png';
+        btnNoteDelete.title = 'Delete';
+        btnNoteDelete.className = 'btn-xs';
+        btnNoteDelete.addEventListener('click', function(){
+            if(confirm('Bener mau dihapus?')){
+                self.deleteItem(self.id)
+            }
+        });
+        toolbarNote.appendChild(btnNoteDelete);
+
+        const btnNoteEdit = new Image(24, 24);
+        btnNoteEdit.src = 'img/edit.png';
+        btnNoteEdit.title = 'Edit';
+        btnNoteEdit.className = 'btn-xs';
+        btnNoteEdit.style.float = 'right';
+        btnNoteEdit.addEventListener('click', function(){
+            self.editItem(self.id, self.text, self.backgroundColor);
+        });
+        toolbarNote.appendChild(btnNoteEdit);
+
+        containerNote.appendChild(toolbarNote);
+
+        const contentNote = document.createElement('div');
+        contentNote.className = 'content-note';
+        contentNote.innerText = this.text;
+
+        containerNote.appendChild(contentNote);
+
+        this.container.appendChild(containerNote);
+    }
+}
+
+export default Note;
---
+++ js/main.js
@@ -0,0 +1,146 @@
+import Note from './Note.js';
+
+'use strict';
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("./serviceworker.js");
+ }
+
+const notes = [];
+let dbNotes;
+let container;
+let lastId = 0;
+let editId = 0;
+
+window.addEventListener('load', function(){
+    container = document.getElementById('container');
+    const bgModal = document.getElementById('bgModal');
+    const modalEdit = document.getElementById('modalEdit');
+    const textEdit = document.getElementById('textEdit');
+
+    const btnAdd = document.getElementById('btnAdd');
+    
+    const btnEditClose = document.getElementById('btnEditClose');
+    const btnEditSave = document.getElementById('btnEditSave');
+
+    const btnColors = document.querySelectorAll('.btnColors');
+
+    btnColors.forEach(item => item.addEventListener('click', function(){
+        textEdit.style.backgroundColor = this.getAttribute('data-color');
+    }));
+
+    btnAdd.addEventListener('click', function(){
+        editId = 0;
+        textEdit.value = '';
+        textEdit.style.backgroundColor = '#fff';
+        showModal(modalEdit, bgModal);
+
+        textEdit.focus();
+    });
+
+    btnEditClose.addEventListener('click', function(){
+        hideModal(modalEdit, bgModal);
+    });
+
+    btnEditSave.addEventListener('click', function(){
+        if(editId == 0){
+            lastId++;
+            let item = {id: lastId, text: textEdit.value, backgroundColor: textEdit.style.backgroundColor, x: 20, y: 20};
+            dbNotes.push(item);
+
+            const note = new Note(item, container, editItem, updatePositionItem, deleteItem);
+            notes.push(note);
+
+            renderAll();
+        }else{
+            notes.forEach((item, index) => {
+                if(item.id == editId){
+                    item.text = textEdit.value;
+                    item.backgroundColor = textEdit.style.backgroundColor;
+    
+                    dbNotes[index].text = item.text;
+                    dbNotes[index].backgroundColor = item.backgroundColor;
+    
+                    localStorage.setItem('notes', JSON.stringify(dbNotes));
+                    renderAll();
+    
+                    return;
+                }
+            });
+
+        }
+        hideModal(modalEdit, bgModal);
+    });
+
+    
+    let stNotes = localStorage.getItem('notes');
+    dbNotes = (stNotes == null) ? [] : JSON.parse(stNotes);
+
+    if(dbNotes.length == 0){
+        let dt0 = {id: 1, text: "Apa aja deh dong\n123\nabc", backgroundColor: '#acf', x: 100, y: 100};
+        dbNotes.push(dt0);
+        let dt1 = {id: 2, text: "Apa aja deh\n123\nabc", backgroundColor: '#fac', x: 500, y: 400};
+        dbNotes.push(dt1);
+        let dt2 = {id: 3, text: "Apa aja dong\n555\nabc", backgroundColor: '#afc', x: 400, y: 100};
+        dbNotes.push(dt2);
+    }    
+
+    const editItem = function(id, text, bgColor){
+        editId = id;
+        textEdit.value = text;
+        textEdit.style.backgroundColor = bgColor;
+        
+        showModal(modalEdit, bgModal);
+
+        textEdit.focus();
+    }
+
+    const updatePositionItem = function(id){
+        notes.forEach((item, index) => {
+            if(id == item.id){
+                dbNotes[index].x = item.x;
+                dbNotes[index].y = item.y;
+
+                localStorage.setItem('notes', JSON.stringify(dbNotes));
+                return;
+            }
+        });
+    }
+
+    const deleteItem = function(id){
+        notes.forEach((item, index) => {
+            if(id == item.id){
+                notes.splice(index, 1);
+                dbNotes.splice(index, 1);
+
+                localStorage.setItem('notes', JSON.stringify(dbNotes));
+                return;
+            }
+        });
+        renderAll();
+    }
+
+    dbNotes.forEach(item => {
+        const note = new Note(item, container, editItem, updatePositionItem, deleteItem);
+        notes.push(note);
+        if(lastId < item.id) lastId = item.id;
+    });
+
+    renderAll();
+});
+function renderAll(){
+    container.innerHTML = '';
+    notes.forEach(note => {
+        note.render();
+    });
+}
+
+function showModal(modal, bg){
+    bg.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+function hideModal(modal, bg){
+    bg.style.display = 'none';
+    modal.style.display = 'none';
+}
